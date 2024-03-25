3736e54335b960582370e8dcc7986d6523d3fd1d
komandu5 <komandu5@msu.edu>
2024-03-25 16:28:48 -0400

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Me, Sudhanv Komanduri!
         </a>
       </header>
     </div>
