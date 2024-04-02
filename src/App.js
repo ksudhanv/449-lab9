92b7cebcf7ee7cf42d44ef1f5d3cf12b603cb6d3
komandu5 <komandu5@msu.edu>
2024-04-01 21:44:36 -0400

    Final Site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+const books = [
+  {id: 1, title: 'One Piece', isFiction: true},
+  {id: 1, title: 'Harry Potter', isFiction: true},
+  {id: 1, title: 'Algebra for dummies', isFiction: false}
+];
+
+function BookShelf(){
+  const listBooks = books.map(book =>
+    <li
+      key={book.id}
+      style={{
+        color: book.isFiction ? 'blue' : 'green'
+      }}>
+      {book.title}
+      </li>
+    );
+    return(
+      <ul>{listBooks}</ul>
+    );
+}
+
+function Welcome(){
+  return (
+    <div>
+      <h1 id="text">Welcome to the Bookshelf</h1>
+    </div>
+    
+  );
+}
+
+function ComeAgain(){
+  return (
+    <div>
+      <h1 id="text">Please Come Again!</h1>
+    </div>
+    
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Me, Sudhanv Komanduri!
-        </a>
+        <Welcome />
+        <BookShelf />
+        <ComeAgain />
       </header>
     </div>
   );
